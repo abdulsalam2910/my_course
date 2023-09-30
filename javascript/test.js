e79eed09fd77fa10b06100514122015c64aee50f
abdulsalam2910 <abdul.sk2910@gmail.com>
2023-09-30 12:28:00 +0530

    add javascript folder

---
+++ javascript/test.js
@@ -0,0 +1,141 @@
+// "let" is key word that declares a variable
+
+// let x declaring
+// let y 
+// x=5   assigning value
+// y=6
+// console.log(x+y)
+
+// let name ="abdul"
+// console.log(name)
+// single quote String
+// let data ='my name is "abdul" and am from vijayawada'
+// console.log(data)
+
+// double quote String
+// let data ="am from 'electrical' batch"
+// console.log(data)
+
+// when given both quotes in one line
+// let data="baby toy's are made up of \"garbage\" plastic"
+// console.log(data)
+// let data='baby toy\'s are made up of "garbage" plastic'
+// console.log(data)
+
+// using of backtics
+// let data=`baby toy's are made up of "garbage" plastic`
+// console.log(data)
+
+// checking of name length
+// let name="abdul salam"
+// console.log(name.length)
+
+// when we redeclare variable it runs in var.
+// var x=5
+// console.log("before",x)
+// var x=6
+// console.log("after",x)
+
+// when we reassign in var it also runs.
+// var x=5
+// console.log("before",x)
+//  x=6
+// console.log("after",x)
+
+// when redeclare in let and run it shows error
+// let x=5
+// console.log("before",x)
+// let x=6
+// console.log("after",x)
+
+// when we reassign variable in let it runs
+// let x=5
+// console.log("before",x)
+//  x=6
+// console.log("after",x)
+
+// in const we connot redeclare and reassign.
+// const x=5
+// console.log("before",x)
+// const x=6
+// console.log("after",x)
+
+// const x=5
+// console.log("before",x)
+//  x=6
+// console.log("after",x)
+
+// for full name
+// let firstName="abdul"
+// let lastName="salam"
+// let fullName=(firstName+lastName)
+// console.log("fullName",fullName)
+
+// for space or any Character between name
+// let firstName="abdul"
+// let lastName="salam"
+// let fullName=(firstName+" "+lastName)
+// console.log("fullName",fullName)
+
+
+// let firstName="abdul"
+// let lastName="salam"
+// let fullName=(firstName+" "+lastName)
+// console.log("fullName",typeof(fullName))
+
+// we can write this in two Ways
+// let num1=5
+// let num2=6
+// console.log(num1+num2)
+
+// let num1=5
+// let num2=6
+// let total=(num1+num2)
+// console.log(total)
+
+// let name="abdul"
+// let num1=10
+// let data=name+num1
+// console.log("total",data)
+// console.log("total",typeof(data))
+
+// let num1=10
+// let name="abdul"
+// let data=num1+name
+// console.log("total",data)
+// console.log("total",typeof(data))
+
+// let num1=5
+// let num2=10
+// let name="abdul"
+// let data=num1+num2+name
+// console.log("total",data)
+// console.log("total",typeof(data))
+
+// number + string is always written as =string
+// string + number is always written as =string
+// number + number is always written as =number
+// string + string is always written as =string
+
+
+// let num1=5
+// let name="abdul"
+// let num2=10
+// let data=num1+name+num2
+// console.log("total",data)
+// console.log("total",typeof(data))
+
+
+            // (or)
+
+
+// let num1=5
+// let name="abdul"
+// let num2=10
+// let data=num1+name+num2
+// console.log("total",data,typeof(data))
+
+
+// let num1=5
+// let num2="10"
+// console.log(typeof(num1+num2))
