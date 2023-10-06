c31660a1c2dd3977f0c0979b364f2beb13084886
abdulsalam2910 <abdul.sk2910@gmail.com>
2023-10-06 09:50:52 +0530

    add folders

--- javascript/string_search.js
+++ javascript/string_search.js
@@ -1,5 +1,80 @@
-let fname1 = "abdul"
-let lname2= "salam"
-let newname3= fname1.concat(lname2).toUpperCase().length
+// index always starts from zero
+// length always starts from 123
 
-console.log(newname3) 
+// let text = "Please locate where 'locate' occurs!";
+// let index = text.indexOf("locate");
+// console.log(index)
+
+// let name = "abdulsalam"
+// console.log(name[7])
+// console.log(name[5])
+
+// array = array is a variable which can hold more than 1 Data and writen in big bracket
+// or set of Data or set of strings like
+
+// let allnames = ["ramakrishna","monika","navyadeepa"]
+
+// let rollno = [5, 14, 16]
+
+// let data = ["abdul","ramakrishna","23","kaki"]
+
+// let allnames = ["ramakrishna","monika","navyadeepa"]
+// console.log(allnames [2]);
+
+// let allnames = ["ramakrishna","monika","navyadeepa"]
+// console.log(allnames [0]);
+
+// let allnames = ["ramakrishna","monika","navyadeepa"]
+// console.log(allnames [1][3]);
+
+// <<procedure to add names
+
+// let allnames = ["ramakrishna","monika","navyadeepa"]
+// console.log(allnames)
+
+// allnames[3] = "jabbar"
+// console.log(allnames)
+
+// allnames[0]="salam"
+// console.log(allnames)
+
+// let allnames = ["ramakrishna","monika","navyadeepa"]
+// allnames.push("abdul","kalyani")
+// console.log(allnames)
+
+// <<procedure to remove names
+
+// let allnames = ["ramakrishna","monika","navyadeepa"]
+// allnames.pop()
+// console.log(allnames)
+
+// <<push is to add the last element and pop is to remove te last element
+
+// let allnames = ["ramakrishna","monika","navyadeepa"]
+// console.log(allnames)
+
+// allnames.unshift("abdul")
+// allnames.push("salam")
+// console.log('afteradding',allnames)
+// allnames.shift()
+// allnames.pop()
+// console.log(allnames)
+
+// <<<to remove the center element in array
+
+// allnames = ["ramakrishna","monika","navyadeepa"]
+// allnames[1]=(" ")
+// console.log(allnames)
+
+// when we don't know the length of the array and we want the last element
+
+// allnames = ["ramakrishna","monika","navyadeepa"]
+// console.log(allnames[1])
+
+// allnames = ["ramakrishna","monika","navyadeepa"]
+// console.log(allnames[allnames.length-1])
+
+
+// string indexof() just explained
+// string last indexof()
+
