3d9ac1540dc43f79225c3174df743488c00636bb
abdulsalam2910 <abdul.sk2910@gmail.com>
2023-10-09 00:01:23 +0530

    modify files

---
+++ javascript/if_else.js
@@ -0,0 +1,66 @@
+// let hours= 10
+// if (hours < 18) {
+//   console.log("Good day")
+// }
+
+// let hours=10
+
+// if (hours>18) {
+//   console.log("good day")
+// }
+// else{
+// console.log("bad day")
+// }
+
+
+// let hours = 15
+
+// if (hours>16){
+//   console.log("abdul is bad boy")
+// }else  { 
+//   console.log("abdul is good boy")}
+
+
+// let hours =15
+
+// if (hours>=18) {
+//   console.log("good day")
+// }
+// else{
+// console.log("bad day")
+// }
+
+
+// let hours = 10
+
+// if (hours<=11){
+//   console.log("good boy")
+// }
+// else{console.log("bad day")}
+
+
+// let time =8
+// if (time < 10) {
+// console.log("Good morning");
+// } else if (time < 20) {
+//   console.log("Good day");
+// } else {
+//   console.log("Good evening");}
+
+
+// let time =15
+// if (time < 10) {
+// console.log("Good morning");
+// } else if (time < 20) {
+//   console.log("Good day");
+// } else {
+//   console.log("Good evening");}
+
+
+// let time =22
+// if (time < 10) {
+// console.log("Good morning");
+// } else if (time < 20) {
+//   console.log("Good day");
+// } else {
+//   console.log("Good evening");}
---
+++ javascript/operators.js
@@ -0,0 +1,89 @@
+
+// Operator	Description
+// +	        Addition
+// -	        Subtraction
+// *	        Multiplication
+// **	        Exponentiation (ES2016) root of
+// /	        Division
+// %	        Modulus (Division Remainder) remainder which we get in Division
+
+
+// + addition is to add the values or numbers
+
+// let x = 7
+// console.log(x)
+
+// x=x+5
+// console.log(x)
+// x+=5
+// x-=5
+// x*=5
+// x/=5
+// x%=5
+// x**=5
+// console.log(x)
+
+
+// <<<Comparison Operators
+
+// let x = 5
+// let y = 2
+// console.log(x+y)
+
+
+// false condition
+// let x = 5
+// let y = 2
+// console.log(x==y)
+
+// true condition
+// let x= 5
+// let y= 5
+// console.log(x==y)
+
+// in this condition operator  checks only values not the type of
+// let x= 5
+// let y="5"
+// console.log(x==y)
+
+
+// in this condition it checks the both the value and type of data
+// let x= 5
+// let y= "5"
+// console.log(x===y)
+
+//<<<not equal to
+// let x= 7
+// let y= 2
+// console.log(x!=y)
+
+// let x= 7
+// let y= "2"
+// console.log(x!=y)
+
+
+// let x= 7
+// let y= "7"
+// console.log(x!=y)
+
+
+// let x= 7
+// let y= "7"
+// console.log(x!==y)
+
+// let x= 2
+// let y= 7
+// console.log(x>y)
+
+// let x= 2
+// let y= 7
+// console.log(x<y)
+
+// let x= 2
+// let y= 7
+// console.log(x>=y)
+
+// let x= 2
+// let y= 7
+// console.log(x<=y)
+
