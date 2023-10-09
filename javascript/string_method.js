8b34a9b4fd1247e397dff21e993e8ad33f688db1
abdulsalam2910 <abdul.sk2910@gmail.com>
2023-10-09 13:57:00 +0530

    modify files

--- javascript/string_method.js
+++ javascript/string_method.js
@@ -192,8 +192,8 @@
 // console.log(uperName)
 
 
-let fname1 = "abdul"
-let lname2= "salam"
-let newname3= fname1.concat(lname2).toUpperCase().length
+// let fname1 = "abdul"
+// let lname2= "salam"
+// let newname3= fname1.concat(lname2).toUpperCase().length
 
-console.log(newname3) 
+// console.log(newname3) 
