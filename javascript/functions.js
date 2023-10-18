4a61f76b38b2747863061e79a92f531ca8ac5cc8
abdulsalam2910 <abdul.sk2910@gmail.com>
2023-10-18 13:59:58 +0530

    new file

---
+++ javascript/functions.js
@@ -0,0 +1,83 @@
+// () - parenthesis
+
+
+// console.log("abdul")
+
+
+// const sum1 =()=>{
+//     console.log("salam")
+// }
+
+// sum1()
+
+
+// the content written in the (brackets) is called parameters
+
+// const sum1 =(name)=>{
+//     console.log(name)
+// }
+// const name = ("ramakrishna")
+// sum1(name)
+
+
+
+// the name given in the globle and for calling need not be same
+// the name given in the globle and function should be same
+
+// const sum1 =(a)=>{
+//     console.log(a)
+// }
+// const name = ("ramakrishna")
+// sum1(name)
+
+// we can directly also send the name
+// const sum1 =(a)=>{
+//     console.log(a)
+// }
+
+// sum1("ramakrishna")
+
+
+// const sum1 =(a)=>{
+//     console.log(a)
+// }
+
+// sum1(1,2)
+
+// for addition
+// const sum1 =(a,b)=>{
+//     console.log("add",a+b)
+// }
+
+// sum1(1,2)
+
+// for substraction
+// const sub =(a,b)=>{
+//     console.log("sub",a-b)
+// }
+// sub(6,2)
+
+
+
+// <<<we can in this way also
+const sum1 =(a,b)=>{
+    console.log(a+b)
+}
+const sub =(a,b)=>{
+    console.log(a-b)
+}
+const multiply =(a,b)=>{
+    console.log(12-3)
+}
+const divide =(a,b)=>{
+    console.log(24/2)
+}
+const reminder =(a,b)=>{
+    console.log(5%5)
+}
+
+sum1(1,2)
+sub(9,3)
+multiply(12,3)
+divide(24,2)
+reminder(2,5)
