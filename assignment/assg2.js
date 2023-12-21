0db083ee29fada5771e9199ffa68ad420bab0fd9
abdulsalam2910 <abdul.sk2910@gmail.com>
2023-12-21 23:34:06 +0530

    add file changes

---
+++ assignment/assg2.js
@@ -0,0 +1,22 @@
+// 1. How do you find the length of a string in JavaScript?
+// let text = "head phones"
+// let length = text.length
+// console.log(length)
+
+// 2. What method is used to convert a string to lowercase in JavaScript?
+
+// function func() {
+//     let str = "RAMA KRISHNA"
+// let string = str.toLowerCase()
+// console.log(string)
+// }
+// func()
+
+        // (or)
+// let text ="I LOVE INDIA"
+// console.log("before",text.toLowerCase())   
+
+// 3. Explain the purpose of the `toUpperCase()` method in JavaScript.
+// it is used change the string to upper case with the help of toUpperCase in javascript
+
+// 4. How can you check if a string contains a specific substring in JavaScript?
